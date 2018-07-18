258b84ea348818e9a4126e91165e42a5cf4507f6
Jonathan Avram <jonathanavram2002@gmail.com>
2018-07-17 23:08:38 -0400

    Initial Commit

---
+++ WeatherApplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApplication
+//
+//  Created by Jonathan Avram on 7/17/18.
+//  Copyright © 2018 Jonathan Avram. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
